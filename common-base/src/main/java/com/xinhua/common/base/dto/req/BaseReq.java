7d9edceb3a4bdc3e37d21c260f94a6f2faa26254
yangfan <yangfan70523@qq.com>
2018-05-30 14:35:09 +0800

    base 文件提交

---
+++ common-base/src/main/java/com/xinhua/common/base/dto/req/BaseReq.java
@@ -0,0 +1,11 @@
+package com.xinhua.common.base.dto.req;
+
+import com.xinhua.common.base.dto.BaseDTO;
+
+/**
+ * @Description
+ * @Author yyf
+ * @DateTime 2018/5/30 14:22
+ */
+public class BaseReq extends BaseDTO {
+}
---
+++ common-base/src/main/java/com/xinhua/common/base/dto/resp/BaseResponse.java
@@ -0,0 +1,11 @@
+package com.xinhua.common.base.dto.resp;
+
+import com.xinhua.common.base.dto.BaseDTO;
+
+/**
+ * @Description
+ * @Author yyf
+ * @DateTime 2018/5/30 14:23
+ */
+public class BaseResponse extends BaseDTO {
+}
