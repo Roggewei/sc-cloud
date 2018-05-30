5be1ca3185277132f12f2faf32a9b35809a5e8e1
yangfan <yangfan70523@qq.com>
2018-05-30 15:24:36 +0800

    日志添加

--- common-base/src/main/java/com/xinhua/common/base/service/BaseService.java
+++ common-base/src/main/java/com/xinhua/common/base/service/BaseService.java
@@ -1,9 +1,13 @@
 package com.xinhua.common.base.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @Description
  * @Author yyf
  * @DateTime 2018/5/30 14:25
  */
 public abstract class BaseService {
+    protected static final Logger logger = LoggerFactory.getLogger(BaseService.class);
 }
